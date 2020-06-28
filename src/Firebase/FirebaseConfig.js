c43a2a3f00fab23a305e81a56bc1df42a87d9336
Monika Mane <manemonika711@gmail.com>
2020-06-28 12:51:23 -0700

    first commit

---
+++ src/Firebase/FirebaseConfig.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyApGeFyhfPeyZk5iAEG-cixae2w4Ln_W18",
+    authDomain: "financial-portfolio-trac-8ef61.firebaseapp.com",
+    databaseURL: "https://financial-portfolio-trac-8ef61.firebaseio.com",
+    projectId: "financial-portfolio-trac-8ef61",
+    storageBucket: "financial-portfolio-trac-8ef61.appspot.com",
+    messagingSenderId: "523920746742",
+    appId: "1:523920746742:web:e32ae6f3ebec01febc24d7",
+    measurementId: "G-PNCF4QQ5Z2",
+  };
+  
